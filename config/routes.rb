8c7f216efeabe38eb406a9126beda2f10099caca
Phil <philipshen13@gmail.com>
2018-07-10 06:25:24 +0000

    Add password reste

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
   root "static_pages#home"
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
@@ -10,6 +8,8 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  
   resources :users
   resources :account_activations, only: ['edit']
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
