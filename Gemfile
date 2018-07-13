0224f0cf7c60eb71bcb234496be486be9d8a8f69
Phil <philipshen13@gmail.com>
2018-07-13 08:49:25 +0000

    Add user microposts

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ gem 'faker',        '1.7.3'
 gem 'bootstrap-sass', '3.3.7'
 gem 'will_paginate', '3.1.6'
 gem 'bootstrap-will_paginate', '1.0.0'
+gem 'carrierwave',  '1.2.2'
+gem 'mini_magick',  '4.7.0'
 gem 'puma',         '3.9.1'
 gem 'sass-rails',   '5.0.6'
 gem 'uglifier',     '3.2.0'
@@ -36,6 +38,7 @@ end
 
 group :production do
   gem 'pg', '0.18.4'
+  gem 'fog', '1.42'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
---
+++ app/uploaders/picture_uploader.rb
@@ -0,0 +1,24 @@
+class PictureUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [400, 400]
+
+  storage :file
+  
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
+
+  # Override the directory where uploaded files will be stored
+  # This is a sensisble default foruploaders that are meant to be mounted
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+  
+  # Add a whitelist of extensions which are allowed to be uploaded
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
+
+end
